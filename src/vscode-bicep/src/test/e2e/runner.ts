a162ef2aa0e7b024292478d661434a73ecfd5319
Shenglong Li <shenglong.li@outlook.com>
2021-02-04 20:18:57 -0800

    Update captureWriteStreams

--- src/vscode-bicep/src/test/e2e/runner.ts
+++ src/vscode-bicep/src/test/e2e/runner.ts
@@ -5,6 +5,7 @@ import * as path from "path";
 // eslint-disable-next-line jest/no-jest-import
 import { runCLI } from "jest";
 import { WriteStream } from "tty";
+import { RevealOutputChannelOn } from "vscode-languageclient";
 
 // https://github.com/microsoft/vscode/blob/87dd7d6a9c2f5fcc2bf3abe555fee79fbbde34bb/src/vs/workbench/api/common/extHostExtensionService.ts#L44
 export type TestRunner = () => Promise<void>;
@@ -12,18 +13,19 @@ export type TestRunner = () => Promise<void>;
 async function captureWriteStreams<T>(
   fn: () => Promise<T>,
   writeStreams: WriteStream[] = [process.stdout, process.stderr]
-): Promise<T> {
+): Promise<[T, string]> {
   const originalWrites = writeStreams.map((ws) => ws.write);
+  const outputs: string[] = [];
 
   try {
     const write = (chunk: Uint8Array | string) => {
-      if (typeof chunk === "string") {
-        console.log(chunk.endsWith("\n") ? chunk.slice(0, -1) : chunk);
+      if (typeof chunk === "string" && chunk.length > 0) {
+        outputs.push(chunk);
       }
       return true;
     };
     writeStreams.forEach((ws) => (ws.write = write));
-    return await fn();
+    return [await fn(), outputs.join()];
   } finally {
     writeStreams.forEach((ws, i) => (ws.write = originalWrites[i]));
   }
@@ -33,9 +35,11 @@ export function createTestRunner(configFile: string): TestRunner {
   return async () => {
     const workspaceRoot = path.resolve(__dirname, "../../..");
     const config = require(path.join(workspaceRoot, configFile)); // eslint-disable-line @typescript-eslint/no-var-requires
-    const { results } = await captureWriteStreams(
+    const [{ results }, outputs] = await captureWriteStreams(
       async () => await runCLI({ _: [], $0: "", ...config }, [workspaceRoot])
     );
+
+    console.log(outputs);
 
     if (results.numFailedTestSuites > 0 || results.numFailedTests > 0) {
       throw new Error(`Tests failed.`);
