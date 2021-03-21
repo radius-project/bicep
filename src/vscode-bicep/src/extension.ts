4bdf671a315d229ee6d6f97e1e4255bfac1f7e79
Ryan Nowak <nowakra@gmail.com>
2021-03-21 14:56:58 -0700

    block usage of official bicep extension with ours

--- src/vscode-bicep/src/extension.ts
+++ src/vscode-bicep/src/extension.ts
@@ -10,6 +10,13 @@ import { createAzExtOutputChannel } from "vscode-azureextensionui";
 export async function activate(
   context: vscode.ExtensionContext
 ): Promise<void> {
+  const stable = vscode.extensions.getExtension('ms-azuretools.vscode-bicep');
+  if (stable !== undefined) {
+    throw new Error(
+      'The Radius Bicep extension cannot be used while the official Bicep extension is also enabled. Please ensure that only one version of the extension is enabled.',
+    );
+  }
+
   const outputChannel = createAzExtOutputChannel("Bicep", "bicep");
 
   await activateWithTelemetryAndErrorHandling(
