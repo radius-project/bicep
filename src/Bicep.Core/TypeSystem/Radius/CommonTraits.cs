077e1d2aedf45325318160bc5786564b11161ac2
Ryan Nowak <nowakra@gmail.com>
2021-05-12 09:29:45 -0700

    Remove extra level of properties from traits

--- src/Bicep.Core/TypeSystem/Radius/CommonTraits.cs
+++ src/Bicep.Core/TypeSystem/Radius/CommonTraits.cs
@@ -5,18 +5,6 @@ namespace Bicep.Core.TypeSystem.Radius
 {
     internal static class CommonTraits
     {
-        public static readonly NamedObjectType DaprTraitPropertiesType = new NamedObjectType(
-            "dapr.io/App@v1alpha1 properties",
-            validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
-            properties: new[]
-            {
-                new TypeProperty("appId", LanguageConstants.String, TypePropertyFlags.Required),
-                new TypeProperty("appPort", LanguageConstants.Int, TypePropertyFlags.None),
-                new TypeProperty("config", LanguageConstants.String),
-            },
-            additionalPropertiesType: LanguageConstants.Any,
-            additionalPropertiesFlags: TypePropertyFlags.None);
-
         public static readonly StringLiteralType DaprTraitKindType = new StringLiteralType("dapr.io/App@v1alpha1");
 
         public static readonly NamedObjectType DaprTraitType = new NamedObjectType(
@@ -25,41 +13,31 @@ namespace Bicep.Core.TypeSystem.Radius
             properties: new[]
             {
                 new TypeProperty("kind", DaprTraitKindType, TypePropertyFlags.Required),
-                new TypeProperty("properties", DaprTraitPropertiesType, TypePropertyFlags.Required),
+                new TypeProperty("appId", LanguageConstants.String, TypePropertyFlags.Required),
+                new TypeProperty("appPort", LanguageConstants.Int, TypePropertyFlags.None),
+                new TypeProperty("config", LanguageConstants.String),
             },
             additionalPropertiesType: LanguageConstants.Any,
             additionalPropertiesFlags: TypePropertyFlags.None);
 
-        public static readonly NamedObjectType IngressTraitPropertiesType = new NamedObjectType(
-            "radius.dev/InboundRoute@v1alpha1 properties",
+        public static readonly StringLiteralType InboundRouteTraitKindType = new StringLiteralType("radius.dev/InboundRoute@v1alpha1");
+
+        public static readonly NamedObjectType InboundRouteTraitType = new NamedObjectType(
+            "radius.dev/InboundRoute@v1alpha",
             validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
             properties: new[]
             {
+                new TypeProperty("kind", InboundRouteTraitKindType, TypePropertyFlags.Required),
                 new TypeProperty("hostname", LanguageConstants.String, TypePropertyFlags.None),
                 new TypeProperty("service", LanguageConstants.String, TypePropertyFlags.Required),
             },
             additionalPropertiesType: LanguageConstants.Any,
             additionalPropertiesFlags: TypePropertyFlags.None);
 
-        public static readonly StringLiteralType IngressTraitKindType = new StringLiteralType("radius.dev/InboundRoute@v1alpha1");
-
-        public static readonly NamedObjectType IngressTraitType = new NamedObjectType(
-            "radius.dev/InboundRoute@v1alpha",
-            validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
-            properties: new[]
-            {
-                new TypeProperty("kind", IngressTraitKindType, TypePropertyFlags.Required),
-                new TypeProperty("properties", IngressTraitPropertiesType, TypePropertyFlags.Required),
-            },
-            additionalPropertiesType: LanguageConstants.Any,
-            additionalPropertiesFlags: TypePropertyFlags.None);
-
-
-
         public static readonly DiscriminatedObjectType TraitType = new DiscriminatedObjectType("trait", TypeSymbolValidationFlags.WarnOnTypeMismatch, "kind", new[]
         {
             DaprTraitType,
-            IngressTraitType,
+            InboundRouteTraitType,
         });
 
         public static readonly TypedArrayType TraitArrayType = new TypedArrayType(TraitType, TypeSymbolValidationFlags.Default);
