149925a55529851046fb763c01372fe30d4dfb01
Ryan Nowak <nowakra@gmail.com>
2021-05-24 19:35:08 -0700

    Ingress -> InboundRoute

--- src/Bicep.Core/TypeSystem/Radius/CommonTraits.cs
+++ src/Bicep.Core/TypeSystem/Radius/CommonTraits.cs
@@ -5,29 +5,6 @@ namespace Bicep.Core.TypeSystem.Radius
 {
     internal static class CommonTraits
     {
-        public static readonly NamedObjectType ManualScalerPropertiesType = new NamedObjectType(
-            "radius.dev/ManualScaler@v1alpha1 properties",
-            validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
-            properties: new[]
-            {
-                new TypeProperty("replicas", LanguageConstants.Int, TypePropertyFlags.Required),
-            },
-            additionalPropertiesType: LanguageConstants.Any,
-            additionalPropertiesFlags: TypePropertyFlags.None);
-
-        public static readonly StringLiteralType ManualScalerKindType = new StringLiteralType("radius.dev/ManualScaler@v1alpha1");
-
-        public static readonly NamedObjectType ManualScalerTraitType = new NamedObjectType(
-            "radius.dev/ManualScaler@v1alpha1",
-            validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
-            properties: new[]
-            {
-                new TypeProperty("kind", ManualScalerKindType, TypePropertyFlags.Required),
-                new TypeProperty("properties", ManualScalerPropertiesType, TypePropertyFlags.Required),
-            },
-            additionalPropertiesType: LanguageConstants.Any,
-            additionalPropertiesFlags: TypePropertyFlags.None);
-
         public static readonly NamedObjectType DaprTraitPropertiesType = new NamedObjectType(
             "dapr.io/App@v1alpha1 properties",
             validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
@@ -54,20 +31,20 @@ namespace Bicep.Core.TypeSystem.Radius
             additionalPropertiesFlags: TypePropertyFlags.None);
 
         public static readonly NamedObjectType IngressTraitPropertiesType = new NamedObjectType(
-            "radius.dev/Ingress@v1alpha1 properties",
+            "radius.dev/InboundRoute@v1alpha1 properties",
             validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
             properties: new[]
             {
-                new TypeProperty("hostname", LanguageConstants.String, TypePropertyFlags.Required),
+                new TypeProperty("hostname", LanguageConstants.String, TypePropertyFlags.None),
                 new TypeProperty("service", LanguageConstants.String, TypePropertyFlags.Required),
             },
             additionalPropertiesType: LanguageConstants.Any,
             additionalPropertiesFlags: TypePropertyFlags.None);
 
-        public static readonly StringLiteralType IngressTraitKindType = new StringLiteralType("radius.dev/Ingress@v1alpha1");
+        public static readonly StringLiteralType IngressTraitKindType = new StringLiteralType("radius.dev/InboundRoute@v1alpha1");
 
         public static readonly NamedObjectType IngressTraitType = new NamedObjectType(
-            "radius.dev/Ingress@v1alpha",
+            "radius.dev/InboundRoute@v1alpha",
             validationFlags: TypeSymbolValidationFlags.WarnOnTypeMismatch,
             properties: new[]
             {
@@ -81,7 +58,6 @@ namespace Bicep.Core.TypeSystem.Radius
 
         public static readonly DiscriminatedObjectType TraitType = new DiscriminatedObjectType("trait", TypeSymbolValidationFlags.WarnOnTypeMismatch, "kind", new[]
         {
-            ManualScalerTraitType,
             DaprTraitType,
             IngressTraitType,
         });
