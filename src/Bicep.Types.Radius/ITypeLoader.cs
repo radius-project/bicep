66553b8b13564d5ac0b9e4afd0958916da271bbf
asilverman <asilverman@microsoft.com>
2022-04-12 10:15:16 -0700

    add radius project and code

---
+++ src/Bicep.Types.Radius/ITypeLoader.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System.Collections.Generic;
+using Azure.Bicep.Types.Radius.Index;
+using Azure.Bicep.Types.Concrete;
+
+namespace Azure.Bicep.Types.Radius
+{
+    public interface ITypeLoader
+    {
+        ResourceType LoadResourceType(TypeLocation location);
+
+        TypeIndex GetIndexedTypes();
+    }
+}
