2e71a596232a1863e9f09b707939671154ad0bda
Marcin Jastrzebski <majastrz@microsoft.com>
2022-04-12 16:23:55 -0700

    register rad namespace

---
+++ src/Bicep.Core/Semantics/Namespaces/RadiusNamespaceType.cs
@@ -0,0 +1,44 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+using System.Collections.Immutable;
+using Bicep.Core.TypeSystem;
+using Bicep.Core.TypeSystem.Radius;
+
+namespace Bicep.Core.Semantics.Namespaces
+{
+    public static class RadiusNamespaceType
+    {
+        public const string BuiltInName = "radius";
+
+        private static readonly IResourceTypeProvider TypeProvider = new RadiusResourceTypeProvider(new RadiusResourceTypeLoader());
+
+        public static NamespaceSettings Settings { get; } = new(
+            IsSingleton: false,
+            BicepProviderName: BuiltInName,
+            ConfigurationType: GetConfigurationType(),
+            ArmTemplateProviderName: "Radius",
+            ArmTemplateProviderVersion: "1.0");
+
+        private static ObjectType GetConfigurationType()
+        {
+            return new ObjectType("configuration", TypeSymbolValidationFlags.Default, new TypeProperty[]
+            {
+                new TypeProperty("foo", LanguageConstants.String, TypePropertyFlags.Required),
+                // new TypeProperty("kubeConfig", LanguageConstants.String, TypePropertyFlags.Required),
+                // new TypeProperty("context", LanguageConstants.String),
+            }, null);
+        }
+
+        public static NamespaceType Create(string aliasName)
+        {
+            return new NamespaceType(
+                aliasName,
+                Settings,
+                ImmutableArray<TypeProperty>.Empty,
+                ImmutableArray<FunctionOverload>.Empty,
+                ImmutableArray<BannedFunction>.Empty,
+                ImmutableArray<Decorator>.Empty,
+                TypeProvider);
+        }
+    }
+}
