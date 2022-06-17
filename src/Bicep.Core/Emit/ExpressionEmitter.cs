68eec5b9c6db49b309565f5de7eea6b954fe5db7
Ariel Silverman <asilverman@users.noreply.github.com>
2022-06-17 15:00:32 -0700

    MAINT: Rebase on azure/bicep/main (#453)

--- src/Bicep.Core/Emit/ExpressionEmitter.cs
+++ src/Bicep.Core/Emit/ExpressionEmitter.cs
@@ -115,15 +115,25 @@ namespace Bicep.Core.Emit
         public void EmitIndexedSymbolReference(DeclaredResourceMetadata resource, SyntaxBase indexExpression, SyntaxBase newContext)
         {
             var expression = converter.CreateConverterForIndexReplacement(resource.Symbol.NameSyntax, indexExpression, newContext)
-                .GenerateSymbolicReference(resource.Symbol.Name, indexExpression);
+                .GenerateSymbolicReference(resource, indexExpression);
 
             writer.WriteValue(ExpressionSerializer.SerializeExpression(expression));
         }
 
+        public void EmitSymbolReference(DeclaredResourceMetadata resource)
+        {
+            var expression = converter.GenerateSymbolicReference(resource, null);
+
+            writer.WriteValue(ExpressionSerializer.SerializeExpression(expression));
+        }
+
+        public string GetSymbolicName(DeclaredResourceMetadata resource)
+            =>  converter.GetSymbolicName(resource);
+
         public void EmitIndexedSymbolReference(ModuleSymbol moduleSymbol, SyntaxBase indexExpression, SyntaxBase newContext)
         {
             var expression = converter.CreateConverterForIndexReplacement(ExpressionConverter.GetModuleNameSyntax(moduleSymbol), indexExpression, newContext)
-                .GenerateSymbolicReference(moduleSymbol.Name, indexExpression);
+                .GenerateSymbolicReference(moduleSymbol, indexExpression);
 
             writer.WriteValue(ExpressionSerializer.SerializeExpression(expression));
         }
@@ -350,11 +360,7 @@ namespace Bicep.Core.Emit
         {
             if (syntax is InstanceFunctionCallSyntax instanceFunctionCall && string.Equals(instanceFunctionCall.Name.IdentifierName, "getSecret", LanguageConstants.IdentifierComparison))
             {
-                var baseSyntax = instanceFunctionCall.BaseExpression switch
-                {
-                    ArrayAccessSyntax arrayAccessSyntax => arrayAccessSyntax.BaseExpression,
-                    _ => instanceFunctionCall.BaseExpression,
-                };
+                var (baseSyntax, _) = SyntaxHelper.UnwrapArrayAccessSyntax(instanceFunctionCall.BaseExpression);
 
                 if (context.SemanticModel.ResourceMetadata.TryLookup(baseSyntax) is not { } resource ||
                     !StringComparer.OrdinalIgnoreCase.Equals(resource.TypeReference.FormatType(), AzResourceTypeProvider.ResourceTypeKeyVault))
@@ -383,14 +389,14 @@ namespace Bicep.Core.Emit
                 writer.WriteEndObject(); // keyVault
 
                 writer.WritePropertyName("secretName");
-                var secretName = converter.ConvertExpression(instanceFunctionCall.Arguments[0].Expression);
+                var secretName = converter.ConvertExpression(instanceFunctionCall.GetArgumentByPosition(0).Expression);
                 var secretNameSerialised = ExpressionSerializer.SerializeExpression(secretName);
                 writer.WriteValue(secretNameSerialised);
 
-                if (instanceFunctionCall.Arguments.Length > 1)
+                if (instanceFunctionCall.Arguments.Count() > 1)
                 {
                     writer.WritePropertyName("secretVersion");
-                    var secretVersion = converter.ConvertExpression(instanceFunctionCall.Arguments[1].Expression);
+                    var secretVersion = converter.ConvertExpression(instanceFunctionCall.GetArgumentByPosition(1).Expression);
                     var secretVersionSerialised = ExpressionSerializer.SerializeExpression(secretVersion);
                     writer.WriteValue(secretVersionSerialised);
                 }
